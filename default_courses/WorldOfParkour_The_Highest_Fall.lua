a2ca179120f29241048246ddfe671b1c10c5b004
Joshua Maldonado <joshjm9915@gmail.com>
2021-03-18 17:20:03 -0400

    Added a new official course

---
+++ default_courses/WorldOfParkour_The_Highest_Fall.lua
@@ -0,0 +1,14 @@
+local _, addon = ...;
+
+-- https://onlinetexttools.com/split-text
+table.insert(addon.defaultCourses, table.concat({
+    "!WOP:1!nn1ZRrrsuymAu3oK1jbsoLdfYcUeYmzYm78RCziruNdUldZmsWto1uv1t)Y0Dvfvv9mmcEr2)aepS)be2JEsuq82EW7flEvbpRN8Ihe",
+    "eF90jrHg6h1RFFVVVV6RVW)mIPsnwX21FktLOJfobpQskWF1fx5F)9oT)WtEzLp)(L60(tT(2xEZwF9ZuJbMsJ5G1rLmXYpLrJJhrztS1F8zhg",
+    "0BMYeZdmjGesO6Gt0cJfBkKUi)vh()Syb1C2xVrph4IfMUkq6i7DyObjsHJYqGOcjDPMjif1RVateThamL8bw4H4Kmd9HZhQx70f9tT8xz4Hri",
+    "G6xpicSe9cWXcLK0Nk5ctKYiVHL0xtL7qCrcYidWhlWsQJGWifmNL0bghLOsLyzxbDcbhL0pnjbCLiblqgFKk3ceOmxknMCCAIgzEy(s3HmxLs",
+    "KcbN4ueu0m1uHjFJcRJy1ztRi2uZuyQyrJq0tlfeu3VuK)I4nXFVDN2VB1xu)Jh)IoTF7)T53E)lFM)sl7xUU)Yb(Re4VAG)xYCwFWg(vo1jR4",
+    "x1)R(RTHVWq)Az(HFD9WbM5l4hihVGI0CMFKGIVmihr7MfLGxuLihajKqvovNrDctPG7JsjbTe35cASaLGkrG6apPKHzqOug9ADbXeO4aGfPKC",
+    "dKAVoW1)wtrTgnBXQuS5Orvl(hTAmQiTSOCXMncB2OwRQTQYO6cXuRlrXHqqW54M1RvU6U71C3k7r2RX(LBUF5Apor4OypAD9MSiQb9EH5wCWH",
+    "k7MlI0lP368g2Jaxu(XhypG5qJo4eWEZtJ8rNG7keyPXU5J(tbhst8ROlmafEwai7A624vYjZuZMMLKvYETkvUuTNZfwMb0o8e9JokcIXqGtP)",
+    "rSHcY8Gd6J55MDYZhC1mzM)nqeFdQmZLvKqmfBJq)gZu5)wsWXZSFh(1yGO39U7T6VFWFPi3oEok0SQ7edCS87)"
+}))
