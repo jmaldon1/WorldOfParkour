9c7ca108814a2d6cd9cf23540008b90a0537a214
Joshua Maldonado <joshjm9915@gmail.com>
2021-03-17 23:02:26 -0400

    added a new official course

---
+++ default_courses/WorldOfParkour_MALDraxxus.lua
@@ -0,0 +1,16 @@
+local _, addon = ...;
+
+-- https://onlinetexttools.com/split-text
+table.insert(addon.defaultCourses, table.concat({
+    "!WOP:1!nsvqVTrruyrBdTSrajveehaXies9sSB8AhhAUe5ekXi1ufftWiUu)2zFo7GNDNrZmRJ3CGd9NqpWj4If34wpHe)gqAufiUbLdGehaj4",
+    "xaVD9giGIeyzPz23mZ33B(EFV5z(KiUk3yXT7(iUkvlrhgNeMlI)YRU81g1FNV9JFD2hm5x7VZV8KN(fF(GF4paJrmfKXcRdY44spIdszeWNy7",
+    "(WZdgC0PkJmoWKkYePGoyUgnwArmZL4VXONWLiyoF3RDKt4KO5qLiZXAT7ydLiRmSebMAm7qWmHsr9nRWKq7bcUk7bwXz0j5g4SIr6vRj6cl5V",
+    "(ODtia1Dg4aJJPYyUeKfbsAlfmWv9PtPl5OC6aTWGLFmemn76VsI)Q1AW39PFwVV512T)o)0VF)x6SFSV)Al5xQR)zd8xpWFJa)ZvEN8bR5xU(",
+    "oe6FE)l4FX18RmYVAzM4VP(32hltIAcFFLXzWzZe21RiFSubor2jSOCbPT0eisnfz9v5w88mCVeLfZAgeCmfRmW(zQuHnHTVbMkCfSHOusxj2j",
+    "evjGLLiojHrJfQCghYQOkJTVkskObPiMGEkAkpYFNqe(V74QJeRYUvjqtxWwcsW5iqmfmjKvLLKQcmh5ayq8hbCQ(sGDk(VeWNYELV)dF8S(78",
+    "Z364YF)Ncy7lra)QHjcjsfosbVO8L))qmUGwOvwRisIRZORW)ulOmNCL2fUbKCi16(HgrksYYGevo5jJqgc2cMyHkLc0rfxATLbsdcXfnnCA0P",
+    "m6vpuGtenEpbprLfBe523qeRFZoHTWWnJB3OtuhOrNn6mUXDcBh2GVvlmER3kS7wJb9ksW6svXIXcmogCOE1nAF7wBD7WwSWWTdBV9MTEyk6aA",
+    "nOR(L5jGb4o0C3yrP3AVQg9ROF1)Ab7qHlzr4E2ECNykgmxy3R(HGK5exJf8CPRi6amwKN6xE(b9U3BBGzZYTZpvD60YgBv2r3P5gn38XXOLBe",
+    "Ahfr)1v9Dvw56EnYMG2AdcJu9lXCxg(a61LfeS(IIxPUEz1K8sV6rhFV7oy7Gwnz3xXEhzbryzus2R2NUYpv(qG98cgqT5wxoFsDj1r)z9SPQS",
+    "tuu5DSIAisOI3IhgzKZaAg8Nd"
+}))
