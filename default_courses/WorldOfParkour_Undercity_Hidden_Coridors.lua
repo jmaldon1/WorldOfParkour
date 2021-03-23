80805f2927e5e80cccad80cfad36feb13537bdd1
Joshua Maldonado <joshjm9915@gmail.com>
2021-03-23 17:35:43 -0400

    added new official course for classic wow

---
+++ default_courses/WorldOfParkour_Undercity_Hidden_Coridors.lua
@@ -0,0 +1,16 @@
+local _, addon = ...;
+
+-- https://onlinetexttools.com/split-text
+table.insert(addon.defaultCourses, table.concat({
+    "!WOP:1!9rvWRXvrCyS1yB20Qjb2cfQW0sjEXDR7MUVKwq3MeRSkeiSnLGiLUZBMz33KDEZmmZ82TVEWd5saXt5GGI9YIEZtckw6Ls)lysrrVju",
+    "X6fpP4XQ(79YUTBGKC7nZ8B(((99977nV0NfsujglBZGDiQyTG5y0OQjC6pC8PU4h1O(t3C2V(T)MnBu)x2E3)(1hS7FHngEpSGYToSKWMqpBF",
+    "LrqJX6BZjk5TT87YkoqZmwOcM0fzI5soCCRhtemSz0fl20XDcMznfx6qvwUTbOF6nYWcPAJwdB6cnwtIbF30w6zgcYyCSdbleHysxBWwJaTqZ8",
+    "MPG)ef8Z0A5iaA9zUHdBCiSdTEKkgBrRQmuSmWFSi)XhPYT)NpU(39(Gkp3Apwp1x5F5j8tu0)kr(t0YFYI(jh2Mv9t5pL)0T8VAr)Rf4Ngyzi",
+    "x(z17Ctld5IyOq1DywuidONUFsrofAZKynsjHVYQTJHLIemAhgchQ6XqrClYgP0LlmcoUSh3Ydfmuwzw4UJXcgOiB1EqaG2H5qj6S9mSY7tK)o",
+    "(w)7NFRFee51MB7)7Do5HjY5pkr(gntKiSqj7mgTjshxGsvj5SFamJUZ3FTTwSr9F(zFXz)Kj)YdJ5lFumFVB0LleTteI098q(ipSTG3jYPXUi",
+    "KlrkzIYO1nPJmdW8jcLn3CYQoVJHe2lA)SghByisIXajwa)S0eLdAe86mhgl7iYRd(traxv1hcY2CigY4bR3D)1l0)E3)dom9w7O0RC9SOGoVq",
+    "(lM6gEEwHyuwlk7NMXSa04EqHmpiZOgQJ9Ls2BrBhszqMSRUFb8K)8p(0N90t3O(p9WLM73Eq8HjGGJqap6dbBngtHaS78gIHHDkZtE3mRmT0Q",
+    "yzxdV7f4u9fj1QuR6cSflDLQvckDzyvPReudxAbgTkJmFiDH2H6PfyRlwr5T5mkf7y6zER5Vu1QxQAfuLfUATfVATABfZCy4mCG(mKiSbtCmZ1",
+    "PCh4aRKp7oM(Cp)a7gCx0EBVKDjIJ3Jvya3UYWxbJgaC1MtseU0LVo2M6NuF2BkPmdH7srn4uktIwrbZdLXoOVQFVS38uYWkLRmF5GVLYSedx7",
+    "GT0V3kq7B5e0gQnku4GFq6nZhkGn1bYGwupMeagc5ONtA5))d"
+}))
