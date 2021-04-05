f6aa1465fe7cdd820eca35a92a3577539f944eec
Joshua Maldonado <joshjm9915@gmail.com>
2021-04-05 00:00:25 -0400

    New official course: Captain of the Antorian Vindicaar

---
+++ default_courses/WorldOfParkour_Captain_of_the_Antoran_Vindicaar.lua
@@ -0,0 +1,18 @@
+local _, addon = ...;
+
+-- https://onlinetexttools.com/split-text
+table.insert(addon.defaultCourses, table.concat({
+    "!WOP:1!Dw5xOXrrCCCABs)ZgSMumAEOItfrksVR3FYDjxa94AAHvinuIPnO9HMjZmB2XB3DgMz2C9keudyFqeKwIT(wRbFrqq0xS4lArOIyEyu",
+    "mTOyl(pkybRGbrefXz27ssZ)fH7HBNF7(7Z3FFNFZVzlxymelwijFyXZJyH8aIIG9ZftXxzBTnCpUL)5RC59pypVQB5FA3VvL)kC6Fdke0jGby",
+    "QubJqKwppcgemgevvwCQfw0zOAmra2resJOHqUZmCIqAcsIu(6Do6xIciqXcVDNdPOQaI4ymAKcK9qEcJqAFeBgampWXGIQgjY3tsonz7uuel6",
+    "us6zmFjsapt9r5D0e09esVJrpKVjHCWZOGcfaQakFcGZekyaqXadtSYlwMUOER(6T1SEVtBBF0dCHjDlF7t2YVC14xq3sR6wlQ3UJEho6D6O3L",
+    "v)ANo1T1uV503NE367VtD7JQ7WsvVh(EhgwL8Fd37D1tm)SgC)WN03ax6JU8MIl)AHBeyqvldlrsuIRTo4s9Kp8HN8nDl)DKx81U71ENnfx3Rb",
+    "USdWIgh88XH8fygYIJuqAKeGcOEEGAuLFsaKVD1vOHbgcmB(NZT8TQFUt)f)(RVPAOWAOHZECjbu30wKOdQrpYQ0GaPvrHwZNQwqCkg3Ai2)6B",
+    "EfaNaRMgy)EyuDGmwimQhBtHnssggN0OzXNqh3xbIiembVyaBAPsalISIcRfNTC8b3VB5B(XN1BwvxBAHvC1fMyqgW21U8e)sNRYFERt6w(BNT",
+    "WDN)oxFttCpRHJ94h1Aljwwny9fnhAtZXuqcgQ6sve2c27jA7R)18ULVXPpYLqF)0wWMSUkE9MWRbS7vyjGbl1UWMGisolyiFcQX0rqOy5iV9)",
+    "CXDnNWyIFZdmWt11FFX1dzPnc5G)V6oWmMWAnnFCDe4p(Ux7dEJ5UUBz9NDt8NEJxEvcCQMZXYSrkS3Lmfys)xJJlmruIdvZmDu2yaIzYxGjIO",
+    "UzyzGC5A5ZNR3h5T)JxXikXxnD6jNFD1s2nslD9SSyJkAmWcb52tYGiwT9jqccuXe8h6PHMb3cyGXktnmf5ZIWcAS8rPy(JvySszW4CLs5H8YM",
+    "Q78flLQuHsft5HjOS5H5X5k0dV9aOufYWupkbJHkcVJmDFWm5oyUSGCz7lxp9LP4uHef0edwK)GMbhcisrehbtvgQ9NCj1w57DXaYrmdzASCfz",
+    "fKIobXzgQS)MxI5pJHLhffhOQp2rjyACOUnoO)MLxZ94krMYdgT0E9m1y1MWExflAOsPZKUW7JjsKGYvMv4tnS9CI5heCysilc4AM7z2WAiJ0o",
+    "onVRYmBy1TxlWAeJtqKlHCFojzTX1WaP9YkdJOv8(haurmUzC()(d"
+}))
