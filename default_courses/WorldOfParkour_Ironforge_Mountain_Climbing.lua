09573a464e7fee4782d0cb7369565051d40916f6
Joshua Maldonado <joshjm9915@gmail.com>
2021-04-06 15:57:53 -0400

    New official classic course: Ironforge Mountain Climbing

---
+++ default_courses/WorldOfParkour_Ironforge_Mountain_Climbing.lua
@@ -0,0 +1,15 @@
+local _, addon = ...;
+
+-- https://onlinetexttools.com/split-text
+table.insert(addon.defaultCourses, table.concat({
+    "!WOP:1!fs5pSrXruyCbMddRfbFw6cfMGgrSOk3IVZ(wUZfCH7WIJizI1gbMuzp3SZ6D8T7mdZmR3CMoKsfsjsu4cxePCPIsIeckqiLI0qbmGIO",
+    "GGsbbNIufzPKM0WS3Fm2aswARM3B)99((MDF7B9MiwSqIxX5wiwepeRWEbfJjE3FOrM4RBu9vFZnxzZxxSr1x83x9c5V3NSfuiiRcd9isfKIWz",
+    "Ufcgg2eIAjDUXGdTCtyIqplreHsIGCRoCSqAkIPQa9Hw6zOqmumO7CUkIkelMNrOkqHA(cZGm6cPeamFW8qrlZiYhRltdTfjigDrjznCoE2(kS",
+    "JZCrc4ATxsp8s1cma5J)vkOqbGkWCgbBdMNqXaxSF8YyBh9(d0d13Q)jk4f)0zVBJQV8cN6b3(Hy9bYOZ4OpOLEyl9HS0hoD01w50J0FulQpI(",
+    "J0hnNE0C6SPcQhtCjgiv1DdEJKG7VXpyap2VCD7FR4EcEQbGnunStbZJVSedKrMWg4ZejM4dSsCexcumqeSfg02esGeyBqmhOciY(nldzCST1x",
+    "y6TxfdfklPnGNMkeQ5sb2nNtR0uWsOabd1Qx93jGw)X)43U5PnFlCN)9ll9phyp9X0VVp052nY)6)Z8Ox6Aq(9d)PF3T)89ezPpq0mUBmfaP9c",
+    "K3A)UgFxQ9QYx55)(w)AJQp9p(VtCQNKDpvZ5dO2XsvJTkwKM8DJZ4OiIY(N9WsKGWvegLFT6HqPKGalWwWYYfdrb7O3bX9ffmQ5YCzmyowmvb",
+    "juBlR6D)Feit)QvAUGaN3O3CmbB5GpZ4qP5TJvsIhoLY2eSpjXJprzhFpCtNI5ptLk48txQSF(YfRujFPYEWMvMCs0uLH8rnJMkI5r8jyppOcZ",
+    "Zo50N28uSaOqHzMC6zkv4gryf0ud6W)yuauarkSywpIIqxU3aUF(X3UGCbIkO3XNtEoKISk2QdrwV)cLGogT8jO4qv7AZcLT1JWh)99oOEijQP",
+    "rGojSKvtxyWOnlyxykBhbsGHkMzjGlb1kSD(Ayk2JGsZ63a"
+}))
