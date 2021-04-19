36f4081f1c16159ab6c148efbb1f142efbf507c8
Joshua Maldonado <joshjm9915@gmail.com>
2021-04-18 23:29:00 -0400

    New official course: King of Zul Aman

---
+++ official_courses/WorldOfParkour_King_of_Zul_Aman.lua
@@ -0,0 +1,16 @@
+local _, addon = ...;
+
+-- https://onlinetexttools.com/split-text
+table.insert(addon.officialCourses, table.concat({
+    "!WOP:1!ns1tOXvryCAJXuFXQjfxRifDueuOUBZ(2KD3MdUKedUA7HYUPwOk08L5nV8gZ8MzyM5TRB8Gsf9IOG9qRG3cONTvevSIErepioOGqbf",
+    "VOvTh9GG387T7wnbxyf2fEVzM3V)nZ8BFxCdQkZyzBx9cuvQwWCSOKWmE0hnX0FwZg)27F9F8nsVwZg)AXxQ7JmL9paJH3berCRdKu2KxGccXg",
+    "aDlB1ZFZbdA1vzerbMuUKNc6GD0mJfNKjDj(dS(3sfmWCZvxOLJ7emZPuCPJuE5ydQJzotocevm5uGzluH6d1hteTZXPk55S8Tzf0ZoKHDnwlQ",
+    "b2U36(PwF5eeqTTTdmocxI4I4JiE2mXdVukijGJ4syeNsNpC(JjCH4rj9uzKU4tejJfHtt0d0abmQmzu)vcggGGsuMiMjFnBYCKmnjHzyLQ63F",
+    "IFIbj4VFFSV8vF2NhtWx82FZ6F1D5VLj9tw1FRb(Pc8hiWFB5jIpOGF6HjqO)G(7WFNf8ZuWpBUp8hs)YpvwkksjYto5DvQiMKeXOkd44kPfhg",
+    "DZZLHge)NhH9xOgDbySLilZiuuYXzO9OmJdqPB5rm7qNBhjO4xqScUg3(69a7XuxA6J(5x5QOPU)p(dPVDNXAQkJWuHNGX0KuylUCZ8m3q6c9i",
+    "GqHVoAxU3G97VNVBFV(CnBC9zw9SN80VZy1W8)xn4p8Ur8gp2ZCTx5tFRMn(L2VW3CPR(xJfXfglI1F3jU31dre)Id(NV3pT1yrS6iYPJ(0m5M",
+    "5BDedZHh7CeapegZeMmBY)EOWOuX7jGUr7pO2R1UxZg)8fV8x)jh5hgl51gb5L))rEmxcIbsWqZxMYOp8tc49DdiW93IRXPjkzKHNzFqEK(HQf",
+    "b1yHluR4XxyoO48vQdfHQXvkwRcuVE5kqyTkL1ZiaRlvfXJ5SOiWX0Zo38hRC1JfwMuoCXWWfRu(8PmhGZbv13nnbma1XmRgXDiRR0VAB)6J8p",
+    "typd3Lmy4LSlrD8oSGD42vgw9LSdYvmNMjC9wUj2r5Nwp7jYpFU7EJD6Q62jVrtjBD8sZvAHRGxKOgUo)qQ(YRLWTe8hqECwQssAMjrMjd6zlf",
+    "e0F(bVrS5Lt28IKNirzDcmCTJQRQeznmL3uHHm(XdRBgM97Q)clyn5DQilTo9jxT9IbRG6vjf9iziBd0tYa9aBWfykXSL(7"
+}))
