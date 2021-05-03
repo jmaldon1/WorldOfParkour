0a672f22abdb7d67d3ce884c407889c4a4acd161
Joshua Maldonado <joshjm9915@gmail.com>
2021-05-02 21:03:41 -0400

    New official course: Scaling Highmountain

---
+++ official_courses/WorldOfParkour_Scaling_Highmountain.lua
@@ -0,0 +1,18 @@
+local _, addon = ...;
+
+-- https://onlinetexttools.com/split-text
+table.insert(addon.officialCourses, table.concat({
+    "!WOP:2!0x32A7C328!vwvxOXrvuyttttCsvtcDnqvlNKg0Qy2ojz3njfXLKQWuKydjLwfFi5M7CNDUKzM749ENSMsbX8GpuFrkQqFtdkO(urWF",
+    "6d(wbX3UurPIOIgHQMALw0hSpiEMz3y62UYIW(W9ND(oFNZ578DB7nwIksKk27x6muryCatZC9hnH7(jT39BFaNY)WNpqPbV4qoLVCYCVI8764",
+    "6ePKVcjWLR0KikRJZqjbblrOlRkT2whAnxvHmW1sgYJ4HKyR1JzsfEjls7B6AXlsdye5w)7CZP56aMCwbpsdJmTNejspNifbq4bZsKlJumUVmm",
+    "r0wGtfrlO4NKLlU36r4MoBoQKCYvx005It7Jag)GZRjsniIaTpxbo8k(HIKinHhb0aUNhWCRWYxYSdFt71Z6FSqpVxXF9RDk)Z3XN(qVL1xz2z",
+    "hMokz2LLPtltxwM7mnlmw5mDxN1JAUlZDBUNCMEYz6nn2M(KpTasjqJaV2lU5P3)p5u(xESZM7uhO)wc8y3oWXtmfG9QLePvhhYkckqICHJZIQ",
+    "WsRNGkMrHQ8GayjgKOyEjbGptER54nE5Vx35(CkVP1aV5X39Z3sQuOju5zoAedQswfWkRwavyPfACH2NbsbkkaEu2MQentcEOwjp8SIeieBdAi",
+    "IXCt)oKKWmmnjuiJ9fOsPrQUXJFH77Z6BtNYxPTl9LpIESws1I3ovn93iKpxx)95NTtNY)27CQf(4V5JAjKLAs2NFgYYmyvuDMvdisux5MLUkU",
+    "llT9KUEl12TKtV0L(I9ETR7u(QR8Np8RFUJ2scmEtiW())203G)TZo)zVbgvZGV7R(7jTmQt0Kk5(AuhDT)y3V2fEkeYtVNf65V2OLqoztGmxJ",
+    "q6CU235LXPKRMmXqNFxF4)nKRv30WUjfN9MPZiyJGRH6AsvsyixpGKkzenk26)ie0sssc4rvg(yCQViYvYtudYDJhAC7cLyEJypmZRO3WfgXES",
+    "HjtoAHHlmbTiBYXjfNC0XJ7jGO0HcxUhN56IQ84ETlCWXSp4OJa22hAKXoKT9AHO2gVJuk(EP(ejHIddpPlxJr9Wz2V7i(()3luNGR9RD8uQPO",
+    "A(kmR15Qdx3E2FDmwECAsGE1LMH5YtcnDhVN5Pzjrd(BRxvuDLuNxr0CtM3oFXpWLPOsESgp5fA7bowQHi(JapbleniDWpdNsR9IqElRS7RTd0",
+    "OuxbXbeuRHZ03CyEuyAPyzweCevatLhSSsR8zArfYkwdten4)M6BfModTDhslIRByGduGwMS9aeurq0qvrsGB(AXGsIavvUM6JQEDvgYHTNisb",
+    "VM9y2SbrHHkAvqZdXeb3LA(uZ(zRx(Q9cr90)F(d"
+}))
