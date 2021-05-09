00a2847d3f959b2ee635df38616aea899517001d
Joshua Maldonado <joshjm9915@gmail.com>
2021-05-09 17:21:00 -0400

    New Official Course: Nashal's Axe

---
+++ official_courses/WorldOfParkour_Nashals_Axe.lua
@@ -0,0 +1,16 @@
+local _, addon = ...;
+
+-- https://onlinetexttools.com/split-text
+table.insert(addon.officialCourses, table.concat({
+    "!WOP:2!0x7E8F6DCF!9rvZVTrruC1wAl1LpsQW0li1NQqQTqIBSJJttaPOK2uLGARQCceXP4NN9Tzh8SZmAMzJJ7nk)hKdCfrecjoaiXPEfo2t",
+    "Ja1luUGeiUWPIuVWb4TooLQiAL8HD23oVFF8E)8r(0UctHZtVxRDfMCRIcuswJczY9o2P)IlSYc)8UBU8F17RwzHhqp4xV)JV3JqNtUnQsK(aQ",
+    "f0XTNPVXPsYr7MsHrVPxEhQ6EwY55VG0HmxUul5YD(rHIq3bxSA7GmOi3TnsDaQVuQJHFSnk7fysHBJUEmXAlC4Dg0Xo(OM8uySRavQUOONV1D",
+    "pOPvApKmvINSsC8olLXT22yTa6cGrdHmcCgrpqrjBrGuZyYyZODl0NHQl4HnWGiRwR4rZIhBKd8WJm2Jpx(NXoW381npXJ(24lC84XRgprw8KD",
+    "IVy14PgjHgXthFP4l3j(kvJVAR4ymdgXJ4zSFY6UbGRqRL6Ta0zk0jdPtj)ljq5ZSecfEiyGujx2BYjiWAKCdzTVwLkRQtjNgv836K9iW4GBsb",
+    "m34Szg2WHlMHBBeG3sIlb9Lkf0LGcpLwOoKQw47FNx7xohRQ)8J)T)48p8zPQPFEQ6TUjYKyapNG(4aOWws(sPem2duvgPYPWHa)TvYZ(5F5kl",
+    "8t)ZF)UR)7XNf4nFEG35gg2l)OIClCXRzk6QOHhMa(asVvPGH2uWryycOXoW1j(CHpBcydEe4V0)dtXDk3jGm2gZqDsnNO82S1E2vXY9yuX3CY",
+    "1LImJoXjl8NxMyFZzAnDJekPXKPPj4KnVskoz36PnMC2MTqCUMZjQFL62XuOpKBsKPskjbdKD8PM5Yt14YnQd1ND(PRp)mZEx2MqUg2Y(6Im0H",
+    "Ia5worgyuV6Wm6rTVXtk43qgY2)1l6xueKBtv2t6V6OmC2EmwPsrHkmyPLr)G4P(HNSKV4o0E9n93UmJA0TNR2u1M57siVWjTb(n27VEPjW)q4",
+    "AuoNCwPqZyc7)xf8E4W67FIxr7rEWQqXq7Bn2XYZiz(eWsotpsdR6vu5Y7hAkG8cFaYl3AKLrYrdbMqagE65XHcKvQ0(9VXYRnFLBzGRRgWos5",
+    "tmYLXPrzhPJPS2ltO)lnr1(3p"
+}))
