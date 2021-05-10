529db83a412e60bd54dc1c03f439f20e3eb91091
Joshua Maldonado <joshjm9915@gmail.com>
2021-05-10 16:56:19 -0400

    New Official Course: Quel'Danas Scaffolding

---
+++ official_courses/WorldOfParkour_QuelDanas_Scaffolding.lua
@@ -0,0 +1,17 @@
+local _, addon = ...;
+
+-- https://onlinetexttools.com/split-text
+table.insert(addon.officialCourses, table.concat({
+    "!WOP:2!0xF08B8AC2!fs13lUvryuT11UE3QUBXu9bRmyvekM0KyZTBxqcnTLnvrzD3vkIp0ozMV7EhYCNzyM5M0SkYA)dWAxuP6t6ck6lcwXFv",
+    "OOp0N0wCqFqOpi2QuWNSiOvqFW5MnPUXgiVDVFZW5C(oZ335wFJgezQ2aVv4QezIIdwGgxoLr)8npXFuV6LttB(sV4luV6V(A)Z7)2V7x87yTM",
+    "1cZPmJfliWyRsWCEdmPPj8e9lgmFBPMtd0jmblbRcwtbAJ)qqyJDJFSVJWbSU)TZnVLz5GEojtyrLQfP96yYJKHasgHMdRB6vOAQEGDugrkoQH",
+    "TmKtTTU8myX5jA8YDIDB5y1I9aQEQfSyTfXeOfJb0bbmfTabRlee8Sgaz91AOLnbbYQb))sucUjG64Pe1g3bLQYQLDnTugvi0TPy3M75nF1flV",
+    "Rt)b1RE1R)Nx7sFCE3TnMBSq3Th42sGB8a3DK1RUGCUj61BLD3P7UC3Do3K5CtLPq320pTeLjZbW9c)4w36dCCpU0zVVVP6YJe3h7MX1LBJiEL",
+    "v2X5p5hTZ6v)5kt83NCLVFKiUNBgr19mlNrbKSfO76hrmnmOFqo71Q)KVQ)Jt)o0V(IF6izPYqybnRSRN7jaRbmQb4ThkQMo16h0sndY45EZF5",
+    "dFpOE1F6ZIfRem1izmCimMV)Cq2aGj71EjWIKIEp7(20yrgcoksYPds)LU6zpZxoUNE4BV(PULxFK0V3Hq)tKrFlqSeeLYrAWRcS9gZHSnOKgP",
+    "m)ENyjKaoUT)uzFH5RpG2UYoWR(kp(p41gzMhEx4ZnsTn9OgIU8N053E5AApIjLF()6rp)irCFdPBlTyxlvG5iv3lXmOS2nZ(LQS99)FtPjzgI",
+    "uRU3dJ9XhAm3xn)ImsSuq1SuZdYOQhIgvoccPH5lvjC687HU3Y5XLi08quXIrLHOPBervtYXgBIKYIyaLITGAQIv2DPI7UCjuPWzQuAMYfprcy",
+    "X(ZWHQTtIXAmXc6drzwpRhOBs5Mu3)noWCeMnE9Y73SFIL1ccwJzoqVK0418CfXiPCBNAhcB64MqT9Njf4pYbXcSjliQFFUwBz7wzPKsX87RqX",
+    "cvodfmentz9vuNAXyVrL5v(eSePavpv4PhTE2TplR75R)hY6hCmifhtGSyVdB4qMX(F86V)ZjtrjP(b7noLnm)3Vfkt9lGlKkAdCoAoU33WPf(",
+    "3d"
+}))
