2d2a6e0638a5f8fa7b5dbb306592f6269cf04e92
Joshua Maldonado <joshjm9915@gmail.com>
2021-05-15 11:00:15 -0400

    New Official Course: Zul'Nazman

---
+++ official_courses/WorldOfParkour_ZulNazman.lua
@@ -0,0 +1,13 @@
+local _, addon = ...;
+
+-- https://onlinetexttools.com/split-text
+table.insert(addon.officialCourses, table.concat({
+    "!WOP:2!0xC0F31AE6!njvqRnrquyS1yRBrTTqSG6HbvuumPjPnBmbKsBTLkwkL0wkup0(YmtYo0D3zyMzBy7nR)d6b)bK)bkiiOEq8pWGhe8KG",
+    "x8U4jp5BtBQfM5W8(EZ33799Ex41TOYeTH)q)JPYivi3YzbvseS3p8y)P6kZ9J78Xh85NFJvM7NV8dFV39(2VbTwCaeYeglet55oMcHHTa6(g)",
+    "Jge0RzxPoK5PJeXIiq51tX1geKhBdCJU3xPHCqpi78nTcBixVUueBjLxOTglKX3oJbISnzDqVpwIQj7ZjY2UcQmExJ4qEE1eNQW5I1KQHdt3Zn",
+    "YEleGeQkSHf0wImMyd4K2HsPMiIrrqXq6Zcgc6oCcdrk67gkWn8GM)VV4DV6l3eB(FLZ)tlpL7I5C58DxYZnINBup3LZAgNxE3yNw8vCxXDv31",
+    "Y7gpVBISsWnP6jBz49vXy1juBIMpq2wjc0hJ7qSsseSpNKktiDH0S3z4wPQOMQ5GvQvt9manpneIFOWMcAGmMPfjMBlyQ7YlxN)4z8HcZWzLkm",
+    "7mTGc1HA1lucAtBvY3VAnwl14HGXgjzI2coJbwUAIsvNUCLPRuMuUwJzl3OuTJI4waXaF11PbGgOwUEjMWIQUy)fLHu36maZ2cBWjHN3mp1koG",
+    "71tyw80fPGEOwTf0KqB6clbMu3y92jj8(RbhgbX96k7Eq2AHmUz9ILkw9TmUHQfklgr9MndegcEaYt5r4WBLKyurYjBRf98wvsHSmBqYOtOFez",
+    "hiMb4Oeb3eDVosimJa0m7WTNZtZS))xgKwPedTVT2pHU46CrcsaoNa8IPKMnuoJOefryrnAU1QlTrdV1KKLdtXF)Vd"
+}))
