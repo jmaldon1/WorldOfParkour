790ccbf56134693d18ec066f2788dda37d351144
Joshua Maldonado <joshjm9915@gmail.com>
2021-05-20 15:06:22 -0400

    New Official Course: Felship Conquerer

---
+++ official_courses/WorldOfParkour_Felship_Conquerer.lua
@@ -0,0 +1,16 @@
+local _, addon = ...;
+
+-- https://onlinetexttools.com/split-text
+table.insert(addon.officialCourses, table.concat({
+    "!WOP:2!0xC6AF6774!9r1tOXvryCS10M(sTnBHTfLwyAOGx6UD)F2nEyztuzvBKysQLEQ78Mz29nzFVzMmZ8YsIGhQfepii9qp4bdn3uXlcIcT",
+    "3LEqyOxS9q7bPhkTEPf8GGq97T)rIv3aV3C47BMVF)((9BMVx66(ezS2WwRY1iYiviZYObfI50FA)t9WInRFVL(Gp7M)1F0S(J8VvJZV9N8mSw",
+    "Z3ahs5glwqytCncom0ht6AQCLrb9wUNuhs90rCbpcR82rX0gijtydCt26oKqgwpA3Px2YTHm9ssUWIYpFBnqKPVysfqY2OLW6UafvhRFnHQDzo",
+    "rkUSHVfCsIgV1MTuPgc0Us5oyR5dGcQQTIfRTiPazdyO1WD6WOiTeimIlaSambusYbfZeajFBwOjGRYwXTVa3(hke3DLZ8rFXK(nR)4AFZZ)Q3",
+    "4NDV8eUJM2DOK(X5L2n1q(xW9kUJ4MULlvA3evChWZDqp3KWAl3XuNArCxgAtODq9WBIIv9X9)fVF9hx)gN6cpTz9Ni(Td0B71hhEf3d80VVeL",
+    "Ob)7cpZVCvZNF4M1)9vUv5x7h(6Xv4s7vJC6bIAeUlx05)0qT5IxOz2AMBCN7)UaM5)2N)Kv3ECywEVW8dVKmg1JhgIemWLSsevIWjQhA5yta4",
+    "MdCrPwW0O1InwKplu2dIYnivFaGdfLybCBcxdyA2lq0hm)RF7BEbGOV6N(D)5hhpoIwzViALvbAWeue3mIuMi4vcIed31GFSTFqRun6QxIIPjA",
+    "g2k1Qt8oy4DIghcABMv5KaPGQ5XMz4u1zkxaxRkRsHm((1QLPu1QKm1YHPzk1U9SfzSCvP5RPMoeBSrskVnNrPyltLkx5ZLV65kKhLF25ko7C5",
+    "lDLiMfd5WvuhNeG1yILPFlk3cOUq)zc7tDY)jH5ICBWGWnmniw(gmVD4MfgoZiyhaR2CsCODt)fzuECKBkvQH3SrlifRhdITENEYEBKmlqkwUw",
+    "2Czl)9uMHO5kler9LRM4uWhg9MSi4fBZybGnAaUz98oVKGt25CO3tl7ghlolQHUtSbsLO5DK4WKJdUChwFZExYSuW2LABgzcjJUgZaGeZBioz)",
+    "7)"
+}))
