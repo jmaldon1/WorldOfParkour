fdf3fdcfd3bb329e130accb31de007e559cb8bbd
Joshua Maldonado <joshjm9915@gmail.com>
2021-06-03 23:16:50 -0400

    New Official Course: Ahn'Qiraj Platforming

---
+++ official_courses/WorldOfParkour_Ahn_Qiraj_Platforming.lua
@@ -0,0 +1,14 @@
+local _, addon = ...;
+
+-- https://onlinetexttools.com/split-text?input=&split-by-char=false&char=%20&split-by-regex=false&regex=%2F%5Cs%2B%2F&split-by-length=true&length=110&separator=%2C%5Cn&symbol-before-chunk=%22&symbol-after-chunk=%22
+table.insert(addon.officialCourses, table.concat({
+    "!WOP:2!0xA2FE3449!fo5tirvqCCCZ)K2tQwT26qbnefbw76B)3t39YIkfRygMiiDj3zNzE(g99EZW8MvtHaZouuhc8qhtyp0TUeb1nJc6uqtf",
+    "brK9hccVviE2AERRscbl84ndZV57NV)M59(UN7xcXklcitzTeI5XDjsc2jzzk(zn1U8WfY)J2KnCNgEzH8p)1p5wnF2nxhke0zGUyAGe6JiTSe",
+    "c66wcIMoWAXTx0y0zzcxSHWJ6t9GCJkCIiqxK4lDuTv8DixcuS9UJoQKkDjIryuFjir)2cDJez8qcaMnyeOyADlY7SktnTjOiM)eb051krc48Z",
+    "vK3rnJ(NsQwl2VJgO4smq4OLQrhvt1ovF)A37xfwOyH8ViYkN(URFfvZTOAXsTxdvRgQ2mu7lSlvgrvTxRRsQ2V6aQdgvfPOQJq2QovhAxi)2B",
+    "(YjMQBNc5F7VJT4hJDZ6ImvDr(1nF4k41wTq(pbn(ZdwB16ImDDr(5B8(JXFAx6VRDn8vzlSuDrMPUixT5R)QLVnRq(F(HnwEWnwUUiT(Fifib",
+    "bkzc(rheQ)jraDP(tgBmkYH5Jf0YbNKI5NYkJnUupzTJHmtKkw60WsX61SxZyjWMPYMgIZwkvsEexyG0JHP2ucgdLeEhMz6ozpDNmbirICMM5m",
+    "Tw0JiH6Aql(rqoqbejjIZJPsTRdunq0i)47uiyCQ0zRL7lOpKKodXOcnyGAbgNkAVSPOYUY5knmbtl7PANhTph)ZCzQaofyexO0MfMgMSYSSzN",
+    "jmmW8hnBCZ4zEmMeGeuUuVc)rxKHGHZYb2rDoWyoeWf0PmIpyincmZ7CGH0xqEyMiUHXyo0aG(rQ3wzFQ)wPkmOMnHzODGfVkSjzq3Qkyap40e",
+    "avgonupt)sai(4DPcada26l1WroliGwYLOn(V)"
+}))
