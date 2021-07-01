f34338733188aa1de881339d04c6a8d14bb4a38f
Joshua Maldonado <joshjm9915@gmail.com>
2021-06-30 23:41:24 -0400

    fixed official course description.

--- official_courses/WorldOfParkour_Boralus_Rooftops.lua
+++ official_courses/WorldOfParkour_Boralus_Rooftops.lua
@@ -2,16 +2,15 @@ local _, addon = ...;
 
 -- https://onlinetexttools.com/split-text?input=&split-by-char=false&char=%20&split-by-regex=false&regex=%2F%5Cs%2B%2F&split-by-length=true&length=110&separator=%2C%5Cn&symbol-before-chunk=%22&symbol-after-chunk=%22
 table.insert(addon.officialCourses, table.concat({
-    "!WOP:2!0x175E8BC8!fw5xOXrrCCCAQXlDpQMK6Pe8FdAP9P76L7F7DHcRj1OBeP0M2AAXk9Mm7CzhZU7moZS54onOelAbFrJfrO9b9a7lL(I4",
-    "dgPuurkITpmeer7dwBTAr9jTwrrP4S3DPyARSW90VFZ9573FZY3FZAERPr0qUa)fLwer9zEyj2XnxiX5dxBYdBBBD(x4Fo9eByD2w)s1TETdF2",
-    "r)niNtMd65qesyac37IiON30q0SIslSsrJjRt5EogCFsaXhYmAXWCHUjoq6Q6R6YipmKVYPtnPKi9W8DqjbsWWJvJRns)tfraqRb2bKpR2ISbB",
-    "Zut7aeen4acst9)eXHnBuLnqxH(pTujQoMRgiBJ7sc5sajaiDXaK2cyEe39eqKna765cHCCMsQECvRT7m)DjU3pH8axZ26NB18QB4mxuDB9Q6T",
-    "K62nujmu9zOwx0mOmsPs21Z5uRxDhQ7mLQ)QQbIuwniZEpcCBfzeppihuNiD74ayqanaO)rKajf4dNfdAONqqDydDzDPOJnDirFjhmZnyTIB9U",
-    "YCO)Y26N(GuS1C)jJ1A5Vfw7bJSwnShGhkCJmsKECQ(or4rz34LH5bp2t33FyBD5tU4vM6q7nwflCZkY3ofe9TyvGVWNHgA7h9k2wxkXqLxAF7",
-    "mwWfVzWQHwfYlU)EENxAPNsZ28SVsRd(9XISu8i)Ox7RlV(jSTo3Vwp3Zm)vJfPz8ip3F(PV8roHT1x(INF3()9BglYYXI8sjAoXjNVST1x9Qp",
-    "SvQKVrSiRelYF4mhzVB(53KT1YF(G78Dh)4))ixOBWnBSm)XcV3r3uZJBBD65)9t82MlhpZHJL5LF9J94(F73yB9X77Emo1sNkEM3Q06J8eHcj",
-    "4zd9zaOhnyMvfbBNneGWajXlkP2jY2j9gDoCGtM33blqCctsObS9)OyFDSYoS9UMT1EbBgdJ2AarCQqC9iNiAv0yuo0lueXBg8QWAy8Kuemc6i",
-    "RCQhI4W2ynKjQeQq20LGMvsxaxaNUItXSPnnnlumxXPDmRGy97bfsFQdPgb74aLy2azlTL8z3sUHb5Yps(8JKR8c(yju3dwID3ixihI0wEChIu",
-    "p3DmEpS776netP3I1P8OIrrsYCyJweX26(YHBlTw1iOqpzJXghkAOsY6FLHBs90kPmrR606Zf9CanyYkzgotwoIJHsQEd)JH1VK4KE3eKlnWHt",
-    "cf)7"
+    "!WOP:2!0xEA8587E8!fw5xOXrrCCCAQXlDpQMK6Pe8FdAP(0D9YL72nxOWAsn6gvkTxBnTOsZKzNl7y2DNXzMnh3PbLyrl4lASicTpOhyFP0xe",
+    "FWiLIksrS9HHGi2(qRwBTO(KwROOuC27UiM2klSp9B(XNVF)nlF)nR5TNgrJ4c8xAUiIgW8XsSRxHiI7hT20h0XX(8V4FFYj2W6CS)5P2Y1o4P",
+    "h9xHCozoOVlriHHiC3lIG((tdrZkmxyLIgvQr5(Ug8asijaYmAYWCH(qCO0t1ZulJ8Xq(kDNPIKi9X8TtjHsWGJvLRnsVtgtaqRc2oKpR2IS(B",
+    "XutBFeenCFcsdCgwFDu4)uRcIdBuFkvQPgZtdKTXDkHCjGecKEyasBbmpM7UdjY6GD(8rqooNPQlp1A7mZFBQ7(tj331CS)PMnU6go1fu3s3QU",
+    "nv3QHkLHQhd16INbLrgv6oEUGA9QBtD7zu9Mr1xSYQ(zo7wGBPiJ47d5GAePxBhaddPHa9hrcKuqaCwmOUEcb1G11L1LIBB6iI(soCMRZAL2YD",
+    "K7a)PJ9p(HzyR5EtNO1g6MyT7p2AvX(aEKWl2iX6XP67eHpLD9xgw7)ipDp)UJ9Lp(IxzYdSNevS4nQiFBuq8)Ivb(7(C0aB7WxXX(IPgy4L27",
+    "oseCPBeSAGvH8cptxV7lV0tPzBD6xT5()(erAMmYp(1)MHx)eo2N9xQv4zN)QjI0kzKN9p(Sx5qhZX(REPZVRG)6Tse5WjI8IPAmXXNFyh7V(1",
+    "Eq7mPFZerworKx6uhApp0lSjh7L)I(3X7n(r))rUqNGB(ez(dfF)dVPgh1X(KZ)Bh7DSwozMdMiZl)gh5Xco3zCS)K9ExgNyPtKmZBwA9HF8iH",
+    "e8Crbma0NgoZQIGTYgcquOK4hNuBhzBNEJ7dh6M7dCXceNWKeAi7jEeCGow5e1AxZwBTGnNHXyuo0ps0cNKYAjxmEKNEfkoCM4EEskcgZyeqNU",
+    "FaIlBJvrwituX8znHwLZwexeNTSBP8zTSSkwQqPPDTkJy96dfYaQlPkb76cLywF5n38q53CHbbfgAKHSgjV1cbyjuFg0KDNAz5qK2HJ7sKAF02",
+    "NDXUN)9aXK6LwTlpQyuKKmh2OjrS1opu41uRvvckYxwFSXHI6Q0SExziR0EifnRrRnx82FAyLY5gmxEoIJHsQEH(JI1pC4MDxeKhn0LtIe)Zp"
 }))
