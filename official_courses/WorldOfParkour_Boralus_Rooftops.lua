3f8e3c6048a45d6c766b58fe196f1c76e049c308
Joshua Maldonado <joshjm9915@gmail.com>
2021-06-30 23:34:46 -0400

    New official course: Boralus Rooftops

---
+++ official_courses/WorldOfParkour_Boralus_Rooftops.lua
@@ -0,0 +1,17 @@
+local _, addon = ...;
+
+-- https://onlinetexttools.com/split-text?input=&split-by-char=false&char=%20&split-by-regex=false&regex=%2F%5Cs%2B%2F&split-by-length=true&length=110&separator=%2C%5Cn&symbol-before-chunk=%22&symbol-after-chunk=%22
+table.insert(addon.officialCourses, table.concat({
+    "!WOP:2!0x175E8BC8!fw5xOXrrCCCAQXlDpQMK6Pe8FdAP9P76L7F7DHcRj1OBeP0M2AAXk9Mm7CzhZU7moZS54onOelAbFrJfrO9b9a7lL(I4",
+    "dgPuurkITpmeer7dwBTAr9jTwrrP4S3DPyARSW90VFZ9573FZY3FZAERPr0qUa)fLwer9zEyj2XnxiX5dxBYdBBBD(x4Fo9eByD2w)s1TETdF2",
+    "r)niNtMd65qesyac37IiON30q0SIslSsrJjRt5EogCFsaXhYmAXWCHUjoq6Q6R6YipmKVYPtnPKi9W8DqjbsWWJvJRns)tfraqRb2bKpR2ISbB",
+    "Zut7aeen4acst9)eXHnBuLnqxH(pTujQoMRgiBJ7sc5sajaiDXaK2cyEe39eqKna765cHCCMsQECvRT7m)DjU3pH8axZ26NB18QB4mxuDB9Q6T",
+    "K62nujmu9zOwx0mOmsPs21Z5uRxDhQ7mLQ)QQbIuwniZEpcCBfzeppihuNiD74ayqanaO)rKajf4dNfdAONqqDydDzDPOJnDirFjhmZnyTIB9U",
+    "YCO)Y26N(GuS1C)jJ1A5Vfw7bJSwnShGhkCJmsKECQ(or4rz34LH5bp2t33FyBD5tU4vM6q7nwflCZkY3ofe9TyvGVWNHgA7h9k2wxkXqLxAF7",
+    "mwWfVzWQHwfYlU)EENxAPNsZ28SVsRd(9XISu8i)Ox7RlV(jSTo3Vwp3Zm)vJfPz8ip3F(PV8roHT1x(INF3()9BglYYXI8sjAoXjNVST1x9Qp",
+    "SvQKVrSiRelYF4mhzVB(53KT1YF(G78Dh)4))ixOBWnBSm)XcV3r3uZJBBD65)9t82MlhpZHJL5LF9J94(F73yB9X77Emo1sNkEM3Q06J8eHcj",
+    "4zd9zaOhnyMvfbBNneGWajXlkP2jY2j9gDoCGtM33blqCctsObS9)OyFDSYoS9UMT1EbBgdJ2AarCQqC9iNiAv0yuo0lueXBg8QWAy8Kuemc6i",
+    "RCQhI4W2ynKjQeQq20LGMvsxaxaNUItXSPnnnlumxXPDmRGy97bfsFQdPgb74aLy2azlTL8z3sUHb5Yps(8JKR8c(yju3dwID3ixihI0wEChIu",
+    "p3DmEpS776netP3I1P8OIrrsYCyJweX26(YHBlTw1iOqpzJXghkAOsY6FLHBs90kPmrR606Zf9CanyYkzgotwoIJHsQEd)JH1VK4KE3eKlnWHt",
+    "cf)7"
+}))
